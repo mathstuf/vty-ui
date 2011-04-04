09ccbf2f1d7882c8d9652acf29123f8ffcb2d986
Ben Boeckel <MathStuf@gmail.com>
2011-04-03 23:42:24 -0400

    Add code to draw a Tree

--- src/Graphics/Vty/Widgets/Tree.hs
+++ src/Graphics/Vty/Widgets/Tree.hs
@@ -16,11 +16,15 @@ module Graphics.Vty.Widgets.Tree (
 
 import Data.Char
 import Data.Char.WCWidth
+import Data.Maybe (isJust, fromMaybe)
 import qualified Data.Tree as T
 import Data.Tree.Path
 
 import Graphics.Vty.Attributes
+import Graphics.Vty.DisplayRegion
+import Graphics.Vty.Picture
 import Graphics.Vty.Widgets.Core
+import Graphics.Vty.Widgets.Skins
 import Graphics.Vty.Widgets.Util
 
 type TreeFormat = String
@@ -52,7 +56,21 @@ instance HasTreeAttr (Widget (Tree a)) where
 -- @showRoot@ is 'False', then the root node is never shown. The attribute
 -- @attr@ is used to color the links showing the hierarchy of nodes.
 newTree :: TreeNode a => Bool -> TreeFormat -> Attr -> T.Tree a -> IO (Widget (Tree a))
-newTree showRoot fmt attr tree = undefined
+newTree showRoot fmt attr tree = do
+    wRef <- newWidget $ \w ->
+        w { state = Tree fmt tree initialPath initialPath showRoot attr
+          , render_ = \this size ctx -> do
+                t <- getState this
+                let img = drawTreeWidget 0 (treeViewTop t) t size ctx
+                    sz = (region_width size, region_height size)
+                return $ crop sz $ pad sz $ img
+          }
+    return wRef
+    where
+        initialPath =
+            if showRoot
+                then pathRoot
+                else pathFirstChild pathRoot
 
 -- | Update the format string of a 'Tree'.
 updateTreeWidgetFormat :: Widget (Tree a) -> TreeFormat -> IO ()
@@ -70,6 +88,51 @@ updateTreeWidgetTree :: Widget (Tree a) -> (T.Tree a -> T.Tree a) -> IO ()
 updateTreeWidgetTree wRef f =
     updateWidgetState wRef $ \t ->
         t { treeData = f (treeData t) }
+
+drawTreeWidget :: TreeNode a => Int -> TreePath -> Tree a -> DisplayRegion -> RenderContext -> Image
+drawTreeWidget line path this size ctx
+    | fromEnum (region_height size) == line = empty_image
+    | otherwise                             =
+        case pathLookup path tree of
+            Just t  -> do
+                let summaryAttr =
+                        if path == treeSelected this
+                            then focusAttr
+                            else normalAttr
+                    entry = string (normalAttr ctx) $ nodeFormat (T.rootLabel t) $ treeFormat this
+                    summary = string (summaryAttr ctx) $ nodeSummary $ T.rootLabel t
+                    fullLine = entry <|> treeLink <|> summary
+                    nextPath =
+                        if nodeExpanded (T.rootLabel t)
+                            then pathFirstChild
+                            else pathAfter
+                fullLine <-> drawTreeWidget (line + 1) (nextPath path) this size ctx
+            Nothing ->
+                case pathParent' path of
+                    Just ppath -> drawTreeWidget line (pathAfter ppath) this size ctx
+                    Nothing    -> empty_image
+        where
+            tree = treeData this
+            treeIndent ipath = fromMaybe "" $ do
+                ppath <- pathParent' ipath
+                _ <- pathParent' ppath
+                let treeLine =
+                        if isJust $ pathLookup (pathAfter ppath) tree
+                            then skinVertical ctxSkin
+                            else ' '
+                Just $ treeIndent ppath ++ (treeLine:' ':' ':[])
+            ctxSkin = skin ctx
+            treeJoin =
+                if isJust $ pathLookup (pathAfter path) tree
+                    then skinIntersectionL
+                    else skinCornerBL
+            treeArrow = treeJoin ctxSkin:skinHorizontal ctxSkin:'>':[]
+            summaryPrefix =
+                let len = pathLength path
+                in if len > 1
+                    then treeIndent path ++ treeArrow
+                    else ""
+            treeLink =  string (treeLinkAttr this) summaryPrefix
 
 -- | Typeclass for a node in a tree widget.
 class TreeNode a where
